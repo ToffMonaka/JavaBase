f4d80009f2323c2f043f74a6d1a54aa2e1be7047
Toff <toff-monaka@live.jp>
2026-05-20 16:35:32 +0900

    修正

--- sample/java_base.bat
+++ sample/java_base.bat
@@ -1,7 +1,7 @@
-@echo Java ベースプロジェクト: 開始
+@echo Java JavaBase: 開始
 
 java -Xms256M -Xmx256M -jar ..\bin\JavaBase.jar
 
-@echo Java ベースプロジェクト: 終了
+@echo Java JavaBase: 終了
 
 pause
---
+++ src/com/toff_monaka/java_base/Main.java
@@ -0,0 +1,28 @@
+/**
+ * @file
+ * @brief Mainファイル
+ */
+
+
+package com.toff_monaka.java_base;
+
+
+import java.io.*;
+
+
+/**
+ * @brief Mainクラス
+ */
+
+public class Main {
+	/**
+	 * @brief main関数
+	 * @param app_param_ary(application_parameter_array)
+	 */
+	public static void main(String[] app_param_ary)
+	{
+		System.out.println("Java Base");
+
+        return;
+	}
+}
